2f06f2d59c4f650b74155110cbf035761e0b9e3c
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-08-19 07:42:49 +0200

    v2.9

--- src/services/domainService.js
+++ src/services/domainService.js
@@ -155,7 +155,7 @@ async function generateAndReplaceKadenaApplicationHaproxyConfig() {
         enviromentParameters: ['CHAINWEB_P2P_PORT=30004', 'CHAINWEB_SERVICE_PORT=30005', 'LOGLEVEL=warn'],
         commands: ['/bin/bash', '-c', '(test -d /data/chainweb-db/0 && ./run-chainweb-node.sh) || (/chainweb/initialize-db.sh && ./run-chainweb-node.sh)'],
         containerData: '/data', // cannot be root todo in verification
-        hash: 'localSpecificationsVersion9', // hash of app message
+        hash: 'localSpecificationsVersion10', // hash of app message
         height: 680000, // height of tx on which it was
       },
       {

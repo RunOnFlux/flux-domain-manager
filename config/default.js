2a64dc4841bcc70472c4161a9d063f2cc590251e
Tadeas Kmenta <kmentat@gmail.com>
2022-03-11 10:18:24 +0700

    add exception for kadena2

--- config/default.js
+++ config/default.js
@@ -40,5 +40,5 @@ module.exports = {
     manageapp: true,
     enabled: true,
   },
-  blackListedApps: ['Kadena', 'firefox', 'firefoxtest', 'firefox2', 'apponflux', 'appononflux', 'testapponflux', 'mysqlonflux', 'mysqlfluxmysql', 'application', 'applicationapplication', 'PresearchNode*', 'FiroNode*'],
+  blackListedApps: ['Kadena', 'Kadena2', 'firefox', 'firefoxtest', 'firefox2', 'apponflux', 'appononflux', 'testapponflux', 'mysqlonflux', 'mysqlfluxmysql', 'application', 'applicationapplication', 'PresearchNode*', 'FiroNode*'],
 };

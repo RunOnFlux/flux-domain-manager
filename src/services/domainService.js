14f20bdb2e36e10030ed43a0bf9c0976143b9ef1
FluxST <st@runonflux.io>
2022-11-21 19:04:01 +0800

    refactor: reuse custom config code

--- src/services/domainService.js
+++ src/services/domainService.js
@@ -8,6 +8,7 @@ const haproxyTemplate = require('./haproxyTemplate');
 const { processApplications, getUnifiedDomains } = require('./domain');
 const { cmdAsync } = require('./constants');
 const applicationChecks = require('./application/checks');
+const { getCustomConfigs } = require('./application/custom');
 
 let myIP = null;
 let myFDMnameORip = null;
@@ -67,75 +68,6 @@ async function generateAndReplaceMainHaproxyConfig() {
       generateAndReplaceMainHaproxyConfig();
     }, 4 * 60 * 1000);
   }
-}
-
-function getCustomConfigs(specifications) {
-  const configs = [];
-  const defaultConfig = {
-    ssl: false,
-    timeout: false,
-    headers: false,
-    loadBalance: false,
-    healthcheck: [],
-    serverConfig: '',
-    enableH2: false,
-  };
-
-  const customConfigs = {
-    '31350.KadefiChainwebNode.KadefiMoneyBackend': {
-      ssl: true,
-      timeout: 90000,
-    },
-    '31350.KadefiPactAPI.KadefiMoneyPactAPI': {
-      ssl: true,
-      healthcheck: ['option httpchk', 'http-check send meth GET uri /health', 'http-check expect status 200'],
-      serverConfig: 'port 31352 inter 30s fall 2 rise 2',
-    },
-    '31351.KadefiPactAPI.KadefiMoneyPactAPI': {
-      timeout: 90000,
-      loadBalance: '\n  balance roundrobin',
-      healthcheck: ['option httpchk', 'http-check send meth GET uri /health', 'http-check expect status 200'],
-      serverConfig: 'port 31352 inter 30s fall 2 rise 2',
-    },
-    '31352.KadenaChainWebData.Kadena3': {
-      timeout: 90000,
-      loadBalance: '\n  balance roundrobin',
-    },
-    '31352.KadefiPactAPI.KadefiMoneyPactAPI': {
-      healthcheck: ['option httpchk', 'http-check send meth GET uri /health', 'http-check expect status 200'],
-      serverConfig: 'inter 30s fall 2 rise 2',
-    },
-    '33952.wp.wordpressonflux': {
-      headers: ['http-request add-header X-Forwarded-Proto https'],
-    },
-    '35000.KadefiMoneyDevAPI.KadefiMoneyDevAPI': {
-      ssl: true,
-      enableH2: true,
-    },
-  };
-
-  let mainPort = '';
-  if (specifications.version <= 3) {
-    for (let i = 0; i < specifications.ports.length; i += 1) {
-      const portName = `${specifications.ports[i]}.${specifications.name}`;
-      if (i === 0) {
-        mainPort = portName;
-      }
-      const appCustomConfig = customConfigs[portName] ? ({ ...defaultConfig, ...customConfigs[portName] }) : defaultConfig;
-      configs.push(appCustomConfig);
-    }
-  } else {
-    for (const component of specifications.compose) {
-      for (let i = 0; i < component.ports.length; i += 1) {
-        const portName = `${component.ports[i]}.${component.name}.${specifications.name}`;
-        const appCustomConfig = customConfigs[portName] ? ({ ...defaultConfig, ...customConfigs[portName] }) : defaultConfig;
-        configs.push(appCustomConfig);
-      }
-    }
-  }
-  const appCustomConfig = customConfigs[mainPort] ? ({ ...defaultConfig, ...customConfigs[mainPort] }) : defaultConfig;
-  configs.push(appCustomConfig);
-  return configs;
 }
 
 async function createSSLDirectory() {

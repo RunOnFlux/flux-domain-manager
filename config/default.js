fcffeb5f81d28f131638ad89481a4458d9513342
Kyuubi2709 <89035963+Kyuubi2709@users.noreply.github.com>
2023-01-24 15:15:13 +0100

    Update default.js

--- config/default.js
+++ config/default.js
@@ -42,7 +42,7 @@ module.exports = {
     manageapp: true,
     enabled: true,
   },
-  mandatoryApps: ['explorer', 'KDLaunch', 'website', 'Kadena3', 'Kadena4', 'HavenNodeMainnet', 'themok', 'themok2', 'themok3', 'themok4', 'themok5'],
+  mandatoryApps: ['explorer', 'KDLaunch', 'website', 'Kadena3', 'Kadena4', 'HavenNodeMainnet', 'themok', 'paoverview'],
   ownersApps: [], // Will retrieve only apps of owners specified here
   whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
   blackListedApps: ['Kadena', 'Kadena2', 'firefox', 'firefoxtest', 'firefox2', 'apponflux', 'appononflux', 'testapponflux', 'mysqlonflux', 'mysqlfluxmysql', 'application', 'applicationapplication', 'PresearchNode*', 'FiroNode*'],

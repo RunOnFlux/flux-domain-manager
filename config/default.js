28c009382a52f15e9903f2ef34117c0a0bc0f45a
TheTrunk <tadeas.kmenta@zel.cash>
2023-01-30 08:52:13 +0800

    separate app config

--- config/default.js
+++ config/default.js
@@ -1,5 +1,6 @@
 const cloudflareConfig = require('./cloudflareConfig');
 const pDNSConfig = require('./PDNSConfig');
+const appsConfig = require('./appsConfig');
 
 module.exports = {
   server: {
@@ -42,8 +43,8 @@ module.exports = {
     manageapp: true,
     enabled: true,
   },
-  mandatoryApps: ['explorer', 'KDLaunch', 'website', 'Kadena3', 'Kadena4', 'HavenNodeMainnet', 'themok', 'paoverview'],
-  ownersApps: [], // Will retrieve only apps of owners specified here
-  whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
-  blackListedApps: ['Kadena', 'Kadena2', 'firefox', 'firefoxtest', 'firefox2', 'apponflux', 'appononflux', 'testapponflux', 'mysqlonflux', 'mysqlfluxmysql', 'application', 'applicationapplication', 'PresearchNode*', 'FiroNode*'],
+  mandatoryApps: appsConfig.mandatoryApps,
+  ownersApps: appsConfig.ownersApps, // Will retrieve only apps of owners specified here
+  whiteListedApps: appsConfig.whiteListedApps, // If there's app in the array, blacklisting will be ignore
+  blackListedApps: appsConfig.blackListedApps,
 };

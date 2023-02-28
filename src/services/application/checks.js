b5f778a7b3aa43938c95917325bf96f025e8d07f
TheTrunk <tadeas.kmenta@zel.cash>
2023-02-28 11:19:01 +0800

    revert

--- src/services/application/checks.js
+++ src/services/application/checks.js
@@ -7,7 +7,7 @@ const serviceHelper = require('../serviceHelper');
 const log = require('../../lib/log');
 
 const timeout = 3456;
-const generalWebsiteApps = ['website', 'AtlasCloudMainnet', 'HavenVaultMainnet', 'KDLaunch', 'paoverview', 'FluxInfo', 'Jetpack2', 'jetpack', 'web'];
+const generalWebsiteApps = ['website', 'AtlasCloudMainnet', 'HavenVaultMainnet', 'KDLaunch', 'paoverview', 'FluxInfo', 'Jetpack2', 'jetpack'];
 
 let currentFluxBlockheight = 1324270;
 

b41e8fb8599810709de8d711cbf1a34dbdb73dbc
FluxST <st@runonflux.io>
2023-02-28 16:24:29 +0800

    feat: add functionality to split apps based on config

---
+++ src/services/application/subset.js
@@ -0,0 +1,56 @@
+const config = require('config');
+const serviceHelper = require('../serviceHelper');
+
+function filterApps(apps, subsetConfig) {
+  const { start, end } = subsetConfig;
+  const startCode = start.charCodeAt(0);
+  const endCode = end.charCodeAt(0);
+  if (startCode > endCode) {
+    throw new Error(`${start} is after ${end} lexigraphically`);
+  }
+
+  const lettersToProcess = {};
+  for (let i = startCode; i <= endCode; i += 1) {
+    const code = String.fromCharCode(i);
+    lettersToProcess[code] = true;
+  }
+
+  const appsInBucket = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const app of apps) {
+    const name = app.name.toLowerCase();
+    if (name.charCodeAt(app.name.length() - 1) in lettersToProcess) {
+      appsInBucket.push(app);
+    }
+  }
+
+  return appsInBucket;
+}
+
+function getApplicationsToProcess(apps, subsetConfig) {
+  let applicationsToProcess = apps;
+
+  // if running apps for a specific owner
+  if (config.ownersApps.length) {
+    return applicationsToProcess.filter((appSpec) => config.ownersApps.includes(appSpec.owner));
+  }
+
+  // exclude not whitelisted apps
+  if (config.whiteListedApps.length) {
+    applicationsToProcess = applicationsToProcess.filter((app) => serviceHelper.matchRule(app.name, config.whiteListedApps));
+  }
+  // exclude blacklisted apps
+  if (config.blackListedApps.length) {
+    applicationsToProcess = applicationsToProcess.filter((app) => !serviceHelper.matchRule(app.name, config.blackListedApps));
+  }
+
+  if (!subsetConfig) {
+    return applicationsToProcess;
+  }
+
+  return filterApps(applicationsToProcess, subsetConfig);
+}
+
+module.exports = {
+  getApplicationsToProcess,
+};

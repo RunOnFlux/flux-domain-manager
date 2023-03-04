5ab979bb2495de83964fbab99c4e10a955dabf6c
TheTrunk <tadeas.kmenta@zel.cash>
2023-03-04 08:39:44 +0800

    adjust list

--- config/appsConfig.js
+++ config/appsConfig.js
@@ -2,5 +2,5 @@ module.exports = {
   mandatoryApps: ['explorer', 'web', 'Kadena3', 'Kadena4', 'themok6', 'paoverview'],
   ownersApps: [], // Will retrieve only apps of owners specified here
   whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
-  blackListedApps: ['Kadena', 'Kadena2', 'firefox', 'firefoxtest', 'firefox2', 'apponflux', 'appononflux', 'testapponflux', 'mysqlonflux', 'mysqlfluxmysql', 'application', 'applicationapplication', 'PresearchNode*', 'FiroNode*'],
+  blackListedApps: ['Kadena', 'Kadena2', 'PresearchNode*', 'BrokerNode*'],
 };

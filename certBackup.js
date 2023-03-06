d1222a44980d667aa6dcdabefcb7910a7868e1f5
TheTrunk <tadeas.kmenta@zel.cash>
2023-03-06 14:26:06 +0800

    cert backup

---
+++ certBackup.js
@@ -0,0 +1,20 @@
+const cmd = require('node-cmd');
+const util = require('util');
+
+const cmdAsync = util.promisify(cmd.run);
+
+const ips = [
+
+];
+
+setInterval(async () => {
+  try {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const ip of ips) {
+      // eslint-disable-next-line no-await-in-loop
+      await cmdAsync(`rsync -avh -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress /etc/ssl/fluxapps/ ${ip}:/etc/ssl/fluxapps/`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}, 5 * 60 * 1000); // every 5 minutes

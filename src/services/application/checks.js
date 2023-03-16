5b54e463087362919d4f010b037291b43636db26
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-03-16 23:39:24 +0100

    added vertcoin in checkBlockBook

--- src/services/application/checks.js
+++ src/services/application/checks.js
@@ -511,8 +511,8 @@ async function checkBitgert(ip, port) {
 
 async function checkBlockBook(ip, port, appsname) {
   try {
-    const coinList = ['litecoin','flux','ethereumclassic'];
-    const addressList = ['LVjoCYFESyTbKAEU5VbFYtb9EYyBXx55V5','t1UPSwfMYLe18ezbCqnR5QgdJGznzCUYHkj','0x0e009d19cb4693fcf2d15aaf4a5ee1c8a0bb5ecf']; 
+    const coinList = ['litecoin','flux','ethereumclassic','vertcoin'];
+    const addressList = ['LVjoCYFESyTbKAEU5VbFYtb9EYyBXx55V5','t1UPSwfMYLe18ezbCqnR5QgdJGznzCUYHkj','0x0e009d19cb4693fcf2d15aaf4a5ee1c8a0bb5ecf','VbFrQgNEiR8ZxMh9WmkjJu9kkqjJA6imdD']; 
     let coin = appsname.replace("blockbook", "");
     coin = coin.replace(/\d+/g, '')
     let index = coinList.indexOf(coin)
@@ -582,4 +582,5 @@ module.exports = {
   checkWanchain,
   checkCelo,
   checkBitgert,
+  checkBlockBook,
 };

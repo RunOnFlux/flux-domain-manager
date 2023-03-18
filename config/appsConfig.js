d666b6d3d925eeed56555d556cabbc52692debc0
TheTrunk <tadeas.kmenta@zel.cash>
2023-03-18 20:21:05 +0800

    add mand app

--- config/appsConfig.js
+++ config/appsConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mandatoryApps: ['explorer', 'web', 'Kadena3', 'Kadena4', 'themok6', 'paoverview'],
+  mandatoryApps: ['explorer', 'web', 'Kadena3', 'Kadena4', 'themok6', 'paoverview', 'wordpress1676076578820'],
   ownersApps: [], // Will retrieve only apps of owners specified here
   whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
   blackListedApps: ['Kadena', 'Kadena2', 'PresearchNode*', 'BrokerNode*'],

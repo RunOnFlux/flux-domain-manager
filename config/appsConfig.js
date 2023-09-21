309ceeee0b99bfb7e557d14585c10562319b7e7f
TheTrunk <tadeas.kmenta@zel.cash>
2023-09-21 17:07:44 -0400

    remove app

--- config/appsConfig.js
+++ config/appsConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mandatoryApps: ['explorer', 'web', 'themok6', 'paoverview', 'eckodexswap', 'HavenNodeMainnet'],
+  mandatoryApps: ['explorer', 'web', 'themok6', 'paoverview', 'eckodexswap'],
   ownersApps: [], // Will retrieve only apps of owners specified here
   whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
   blackListedApps: ['Kadena', 'Kadena2', 'PresearchNode*', 'BrokerNode*', 'Folding*'],

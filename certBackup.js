ab308a5b9b9a3cbec1f821af928a3f66287c1eba
TheTrunk <tadeas.kmenta@zel.cash>
2023-10-14 17:20:42 +0200

    use update mode for rsync

--- certBackup.js
+++ certBackup.js
@@ -12,7 +12,7 @@ setInterval(async () => {
     // eslint-disable-next-line no-restricted-syntax
     for (const ip of ips) {
       // eslint-disable-next-line no-await-in-loop
-      await cmdAsync(`rsync -avh -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress /etc/ssl/fluxapps/ ${ip}:/etc/ssl/fluxapps/`);
+      await cmdAsync(`rsync -avh -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --update --progress /etc/ssl/fluxapps/ ${ip}:/etc/ssl/fluxapps/`);
       console.log(`Certs sent to ${ip}`);
     }
   } catch (error) {

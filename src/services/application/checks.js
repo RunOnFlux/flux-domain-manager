16fe9e93400e5e5b4105565fec05aa1ab2fcc7bc
Cabecinha84 <valter.cabecinha@gmail.com>
2024-01-25 10:01:42 +0000

    update fluxOs min. version to 4.27.2

--- src/services/application/checks.js
+++ src/services/application/checks.js
@@ -76,7 +76,7 @@ async function isVersionOK(ip, port) {
     const url = `http://${ip}:${port}/flux/info`;
     const response = await serviceHelper.httpGetRequest(url, timeout);
     const version = response.data.data.flux.version.replace(/\./g, '');
-    if (+version >= 4270) {
+    if (+version >= 4272) {
       if (response.data.data.flux.development === 'false' || !response.data.data.flux.development) {
         return true;
       }

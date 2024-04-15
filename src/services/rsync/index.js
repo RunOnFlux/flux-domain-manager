90eda2995db59a838b941262d0ce5247e02fd385
David White <david@runonflux.io>
2024-04-15 12:43:04 +0100

    Add future rsync setup, update cmd

--- src/services/rsync/index.js
+++ src/services/rsync/index.js
@@ -1,21 +1,22 @@
 const cmd = require('node-cmd');
 const util = require('util');
 const { getHostsToRsync } = require('./config');
+const log = require('../../lib/log');
 
 const cmdAsync = util.promisify(cmd.run);
 
 async function startCertRsync() {
-  console.log('starting r sync');
+  log.info('starting rsync');
   const ips = getHostsToRsync();
   try {
     // eslint-disable-next-line no-restricted-syntax
     for (const ip of ips) {
       // eslint-disable-next-line no-await-in-loop
-      await cmdAsync(`rsync -avh -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress /etc/ssl/fluxapps/ ${ip}:/etc/ssl/fluxapps/`);
-      console.log(`Certs sent to ${ip}`);
+      await cmdAsync(`rsync -au -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" /etc/ssl/fluxapps/ ${ip}:/etc/ssl/fluxapps/`);
+      log.info(`Certs sent to ${ip}`);
     }
   } catch (error) {
-    console.log(error);
+    log.info(error);
   }
 }
 

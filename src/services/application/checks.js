7ba61e89f5203366d91c20bf569e8cb424a06b64
TheTrunk <tadeas.kmenta@zel.cash>
2024-05-17 18:48:34 +0700

    force 5.6.0

--- src/services/application/checks.js
+++ src/services/application/checks.js
@@ -111,7 +111,7 @@ async function isVersionOK(ip, port) {
     const url = `http://${ip}:${port}/flux/info`;
     const response = await serviceHelper.httpGetRequest(url, timeout);
     const version = response.data.data.flux.version;
-    if (minVersionSatisfy(version, '5.8.0')) {
+    if (version === '5.6.0') {
       if (response.data.data.flux.development === 'false' || !response.data.data.flux.development) {
         return true;
       }

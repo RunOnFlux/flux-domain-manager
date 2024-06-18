62db948c04d259798074bc73b5ab3b2a55769402
TheTrunk <tadeas.kmenta@zel.cash>
2024-06-18 14:20:36 +0800

    block app

--- config/appsConfig.js
+++ config/appsConfig.js
@@ -2,6 +2,6 @@ module.exports = {
   mandatoryApps: ['explorer', 'web', 'themok6', 'paoverview', 'HavenNodeMainnet', 'HavenVaultMainnet', 'HavenExplorerMainnet'],
   ownersApps: [], // Will retrieve only apps of owners specified here
   whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
-  blackListedApps: ['Kadena', 'Kadena2', 'PresearchNode*', 'BrokerNode*', 'Folding*'],
+  blackListedApps: ['Kadena', 'Kadena2', 'PresearchNode*', 'BrokerNode*', 'Folding*', 'corsanywhere'],
   minecraftApps: ['mcf', '*minecraft*', '*Minecraft*'],
 };

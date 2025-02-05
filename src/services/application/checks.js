bd9668ac97f8ad39f5a503dbc12b6fcf1023fabc
Cabecinha84 <valter.cabecinha@gmail.com>
2025-02-05 16:31:55 +0000

    bump min. fluxos version

--- src/services/application/checks.js
+++ src/services/application/checks.js
@@ -112,7 +112,7 @@ async function isVersionOK(ip, port) {
     const url = `http://${ip}:${port}/flux/info`;
     const response = await serviceHelper.httpGetRequest(url, timeout);
     const version = response.data.data.flux.version;
-    if (minVersionSatisfy(version, '5.38.1')) {
+    if (minVersionSatisfy(version, '5.39.0')) {
       if (response.data.data.flux.development === 'false' || !response.data.data.flux.development) {
         return true;
       }

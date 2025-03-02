e64fead2aa34ee6cb07ec102e0750ed1c0559a9d
TheTrunk <tadeas@runonflux.io>
2025-03-03 07:16:10 +0800

    remove haven

--- config/appsConfig.js
+++ config/appsConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mandatoryApps: ['explorer', 'web', 'themok6', 'paoverview', 'HavenNodeMainnet', 'HavenVaultMainnet', 'HavenExplorerMainnet'],
+  mandatoryApps: ['explorer', 'web', 'themok6', 'paoverview'],
   ownersApps: [], // Will retrieve only apps of owners specified here
   whiteListedApps: [], // If there's app in the array, blacklisting will be ignore
   blackListedApps: ['Kadena', 'Kadena2', 'PresearchNode*', 'BrokerNode*', 'Folding*', 'corsanywhere'],

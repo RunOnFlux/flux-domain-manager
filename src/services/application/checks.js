4ec73989565397d9678b9484ad7d82fc7e3dc3c6
TheTrunk <tadeas@runonflux.io>
2025-06-04 17:32:46 +0800

    fix check

--- src/services/application/checks.js
+++ src/services/application/checks.js
@@ -203,7 +203,7 @@ async function hasManyMessages(ip, port) {
     const appsAmount = response.data.data.length;
     if (appsAmount > 48000) {
       const messageFalse = response.data.data.filter((a) => a.message === false);
-      if (messageFalse.length < 80) {
+      if (messageFalse.length < 100) {
         return true;
       }
     }
